c9146860a8d6883f3c61ce41042b5e88416c9206
Konrad Gorecki <konradgorecki7@gmail.com>
2021-03-22 23:50:00 +0100

    [CLEANUP] frontend code cleanup - rename of packages and other similar updated

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,29 +1,31 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import {AppHeaderComponent} from './modules/header/app-header/app-header.component';
+import {AppRoutingModule} from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
-import {MainPageComponent} from './modules/main-page/main-page.component';
-import {FooterComponent} from './modules/footer/footer.component';
-import {SearchFormComponent} from './modules/search-form/search-form.component';
-import {ReactiveFormsModule} from "@angular/forms";
 import {MatStepperModule} from "@angular/material/stepper";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatChipsModule} from "@angular/material/chips";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
-import {SelectCountriesFormComponent} from './modules/select-countries-form/select-countries-form.component';
-import { SelectDevicesFormComponent } from './modules/select-devices-form/select-devices-form.component';
-import { SearchExecutorComponent } from './modules/search-executor/search-executor.component';
 import {MatExpansionModule} from "@angular/material/expansion";
 import {MatCardModule} from "@angular/material/card";
+
+import {AppComponent} from './app.component';
+import {MainPageComponent} from './components/main-page/main-page.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {AppHeaderComponent} from './components/header/app-header/app-header.component';
+import {SearchFormComponent} from './components/search-form/search-form.component';
+import {SelectCountriesFormComponent} from './components/select-countries-form/select-countries-form.component';
+import { SelectDevicesFormComponent } from './components/select-devices-form/select-devices-form.component';
+import { SearchExecutorComponent } from './components/search-executor/search-executor.component';
 
 @NgModule({
   declarations: [
